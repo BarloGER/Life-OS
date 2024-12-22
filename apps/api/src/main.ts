018428950ca08851ce90c4bbe93fca4088251139
Mike Barlozewski <108719328+BarloGER@users.noreply.github.com>
2024-12-22 11:58:48 +0100

    chore: add gitignore, ts paths. dependencies etc

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,27 @@
 import { initializeServer } from '@shared/frameworks/express';
+import { pgClient } from '@shared/frameworks/db/postgres';
+import { NotificationService } from '@shared/services/notification/NotificationService';
+import { PBKDF2PasswordHasher } from '@shared/utils/password-hasher/PBKDF2PasswordHasher';
+import { TokenGenerator } from '@shared/utils/token-generator/TokenGenerator';
 
 export type AppDependencies = {
-  message: string;
+  pgClient;
+  notificationService: NotificationService;
+  passwordHasher: PBKDF2PasswordHasher;
+  tokenGenerator: TokenGenerator;
 };
 
 export function buildAppDependencies(): AppDependencies {
-  return;
+  const notificationService = new NotificationService();
+  const passwordHasher = new PBKDF2PasswordHasher();
+  const tokenGenerator = new TokenGenerator();
+
+  return {
+    pgClient,
+    notificationService,
+    passwordHasher,
+    tokenGenerator,
+  };
 }
 
 initializeServer(buildAppDependencies());
